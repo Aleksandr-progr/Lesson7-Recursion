d58fef9e4c53b4d30ca66339cda7c505c140d8fa
Alekandr-prog <a.a.sergeev1978@yandex.ru>
2024-02-10 00:04:18 +0300

    Изменения в файле Program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Основной файл программы
+namespace Tasks
+{
+    // Класс, содержащий точку входа в программу и выполнение задач
+    static class Program
+    {
+        // Главный метод программы
+        public static void Main(string[] args)
+        {
+            // Очистка консоли перед выполнением задачи
+            Console.Clear();
+            // Вывод приглашения для выбора задачи
+            Console.Write(TaskSelectionPrompt);
+            // Получение номера выбранной задачи от пользователя и выполнение соответствующей задачи
+            int selectedTask = NumberInTerminal(3, TaskSelectionPrompt, 1);
+            ExecuteTask(selectedTask);
+        }
+
+        // Метод для выполнения выбранной задачи
+        static void ExecuteTask(int taskNumber)
+        {
+            switch (taskNumber)
+            {
+                case 1:
+                    // Выполнение задачи 1
+                    Task1.Execute();
+                    break;
+                case 2:
+                    // Выполнение задачи 2
+                    Task2.Execute();
+                    break;
+                case 3:
+                    // Выполнение задачи 3
+                    Task3.Execute();
+                    break;
+                default:
+                    // Вывод сообщения об ошибке при неверном выборе задачи
+                    Console.WriteLine("Неверный номер задачи.");
+                    break;
+            }
+        }
+
+        // Другие методы программы и константы здесь
+    }
+}
